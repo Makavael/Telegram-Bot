245f4a72bf8404dd93fbe23afeb8893bf729b150
Makavael <151890083+Makavael@users.noreply.github.com>
2024-10-13 05:57:06 +0300

    Bot.py
    https://t.me/Makavael
    https://t.me/walletveli
    https://t.me/walletveli_bot

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import threading
 from random import choice, randint
 import colorama
 import asyncio
+import emoji
 
 colorama.init(autoreset=True)
 
@@ -17,30 +18,49 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Hello! I am your bot.')
 
 def generate_visa():
-    bin_number = '455225'
+    bin_numbers = ['455225', '537410', '414763', '434257', '519699']
+    bin_number = choice(bin_numbers)
     numofvisa = 16
     nums = '0123456789'
+    
+    
     visa = bin_number + ''.join(choice(nums) for _ in range(numofvisa - len(bin_number)))
 
+    
     expiry_year = randint(2024, 2030)
     expiry_month = randint(1, 12)
-    expiry = f"{expiry_month:02d} l {expiry_year}"
+    expiry = f"{expiry_month:02d}|{expiry_year}"  
 
     cvc = ''.join(choice('0123456789') for _ in range(3))
 
+    countries = {
+        "USA": "🇺🇸",
+        "Canada": "🇨🇦",
+        "UK": "🇬🇧",
+        "Germany": "🇩🇪",
+        "France": "🇫🇷",
+        "Italy": "🇮🇹",
+        "Spain": "🇪🇸",
+        "Australia": "🇦🇺",
+        "Egypt": "🇪🇬",
+        "Saudi Arabia": "🇸🇦",
+        "United Arab Emirates": "🇦🇪",
+        "Japan": "🇯🇵",
+        "China": "🇨🇳",
+        "India": "🇮🇳",
+    }
+
+    country_name = choice(list(countries.keys()))
+    country_flag = countries[country_name]
+
     message = (
-        "Dev : @Makavael\n"
-        "↳\n"
-        f"Card: {visa} l {expiry} l {cvc}\n"
-        "↳\n"
-        f"Bin Information : (#{bin_number})\n"
-        "↳ Vendor: Visa\n"
-        "↳ Type: DEBIT\n"
-        "↳ Level: CLASSIC\n"
-        "↳ Bank:\n"
-        " ↳ USA\n"
-        "↳ Country:\n"
-        " ↳ UNITED STATES - USD - 🇺🇸"
+        "Approved!✅\n\n"
+        f"• 𝗖𝗔𝗥𝗗 : {visa}|{expiry}|{cvc}\n"
+        "• 𝐆𝐚𝐭𝐞𝐰𝐚𝐲 : Stripe Auth 2!!\n"
+        "• 𝗥𝗘𝗦𝗣𝗢𝗡𝗦𝗘 : [CVV CARD - Status -> Succeeded!]\n\n"
+        "• 𝗜𝗡𝗙𝗢: : VISA - DEBIT - VISA CLASSIC\n"
+        f"• 𝗜𝗦𝗦𝗨𝗘𝗥 : Green Dot Bank Dba Bonneville Bank\n"
+        f"• 𝗖𝗼𝘂𝗻𝘁𝗿𝘆 : {country_name} {country_flag}"
     )
 
     return message
@@ -55,7 +75,7 @@ async def post_to_channel(message):
 
 def job():
     visa_message = generate_visa()
-    asyncio.run(post_to_channel(visa_message))  # استخدام asyncio.run
+    asyncio.run(post_to_channel(visa_message))  
 
 def run_scheduler():
     schedule.every(5).seconds.do(job)  
